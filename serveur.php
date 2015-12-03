15c075383297997de787dad95a75866713f89e14
Gweedzy <pj.leborgne@gmail.com>
2015-12-03 21:40:51 +0100

    1
    qsd

---
+++ serveur.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Notre Serveur Médiéval RP - Nether News</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="serveurstyle.css">
+    </head>
+    
+    <body>
+        <div id="megatop">
+            <img class="logoserv" alt="logoserv">
+            <p class="servname">MEDIEVAL RP</p>
+        </div>
+        
+        <div id="top">
+            
+        </div>
+        
+        <div id="mid">
+            <div id="content">
+                <div class="category">
+                    <img src="shop.svg" alt="shopimg" class="blockimg">
+                    <p class="blocktitle">Une boutique disponible</p>
+                    <p class="blocktext">Achetez un rang (Baron, Vicomte..) et bénéficiez d'avantages intéressants comme un salaire plus élevé, des armes et outils plus efficaces ou même de                                          l'argent. Vous pouvez également acheter des animaux pour votre ferme (cochons, vaches, poules, moutons). Toute chose achetée dans la boutique sera                                            disponible dans les 24h sur le jeu.
+                                            <a href="#" class="blocktextlink" target="_blank">Visiter la boutique >></a>                       
+                    </p>
+                </div>
+                
+                <div class="category">
+                    <img src="community.svg" alt="communityimg" class="blockimg">
+                    <p class="blocktitle">Une communauté agréable</p>
+                    <p class="blocktext">Venez jouer sur notre serveur & partagez de bons moments avec une communauté sympathique et plaisante dans laquelle vous serez parfaitement intégré.                                          L'entraide et la bonne humeur sont deux choses primordiales pour que les nouveaux joueurs se sentent bien sur le serveur. Il y a quelques <a href="#"                                          target="_blank" class="blocktextlink">REGLES</a> à respecter, il est d'ailleurs conseillé de les <a href="#" target="_blank"class="blocktextlink">
+                                         LIRE</a> avant de venir sur le serveur pour ne pas déranger les joueurs, pour ne pas recevoir de ban (temporaire ou permanent selon l'acte). 
+                                            <a href="#" class="blocktextlink" target="_blank">Voir la liste des membres >></a>
+                    </p>
+                </div>
+                
+                <div class="category">
+                    <img src="help.svg" alt="helpimg" class="blockimg">
+                    <p class="blocktitle">Un staff à votre écoute</p>
+                    <p class="blocktext">Parce qu'on a toujours un petit soucis ou une question à poser, des modérateurs et des administrateurs vous écouteront parler et feront de leur mieux                                          pour le régler votre problème, ou répondre à votre question. Vous avez la possibilité de contacter le staff sur le serveur via un chat spécial, à                                              utiliser avec modération, en précisant la raison du problème, comme un joueur qui ne respecte pas les règles ou encore si vous êtes coincé ou que                                              vous trouvez un bug sur le serveur. <b>LE STAFF VOUS RESPECTE, MERCI DE LE RESPECTER EN RETOUR.</b>
+                                            <a href="#" class="blocktextlink" target="_blank">Accéder au support >></a>
+                    </p>
+                </div>
+                
+                <div class="category">
+                    <img src="events.svg" alt="eventsimg" class="blockimg">
+                    <p class="blocktitle">Des évenements fréquents</p>
+                    <p class="blocktext">Diverses animations seront organisées sur le serveur, de simples concours, tirages au sort, aux batailles les plus sanglantes, aux salaires plus                                              élevés pendant une heure et même d'autres évenements. Vous pouvez proposer des idées d'évènement via la boîte à idées sur le serveur ou celle du                                              site!
+                                            <a href="#" class="blocktextlink" target="_blank">Accéder à la boite à idée >></a>
+                    </p>
+                </div>
+                
+                <div class="category2">
+                    <img src="security.svg" alt="securityimg" class="lineimg">
+                    <p class="linetitle">Fiabilité & sécurité</p>
+                    <p class="linetext"><b>SERVEUR : </b>Notre serveur possède une bonne protection contre les tricheurs et contre tout joueur tentant d'utiliser quelconque "hack".                                                    Cependant, certains tricheurs réussiront à la contourner et nous comptons sur vous pour nous les signaler. Ces derniers seront bannis du serveur.                                              <br><br>
+                                        <b>SITE : </b>Les transactions sont également sécurisées, vous pouvez ainsi utiliser la boutique sans crainte.</p>
+                </div>
+                </div>
+            
+            <div class="separator"></div>
+            
+            <div id="container2">
+                
+            </div>
+        </div>
+                
+    </body>
+</html>
